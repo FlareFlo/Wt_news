5969ff270f4927fca68bff64c096b50a62128b4d
FlareFlo <mail@flareflo.dev>
2022-03-17 15:19:14 +0100

    Bumped SDK version

--- app/src/main/java/com/example/wt_news/MainActivity.kt
+++ app/src/main/java/com/example/wt_news/MainActivity.kt
@@ -40,27 +40,6 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-       showNotification("Sus!", "ok")
-    }
-
-    fun showNotification(title: String, message: String) {
-        val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            val channel = NotificationChannel("i.apps.notifications",
-                "i.apps.notifications",
-                NotificationManager.IMPORTANCE_DEFAULT)
-            channel.description = "YOUR_NOTIFICATION_CHANNEL_DESCRIPTION"
-            mNotificationManager.createNotificationChannel(channel)
-        }
-        val mBuilder = NotificationCompat.Builder(applicationContext, "YOUR_CHANNEL_ID")
-            .setSmallIcon(R.mipmap.ic_launcher) // notification icon
-            .setContentTitle(title) // title for notification
-            .setContentText(message)// message for notification
-            .setAutoCancel(true) // clear notification after click
-        val intent = Intent(applicationContext, ACTIVITY_NAME::class.java)
-        val pi = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-        mBuilder.setContentIntent(pi)
-        mNotificationManager.notify(0, mBuilder.build())
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
